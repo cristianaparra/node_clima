cd6231054ff56c4aea933ab2003702028f4dd7f8
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-08 19:03:11 -0300

    metodo para capitalizar y finalizacion de programa en node

--- app.js
+++ app.js
@@ -47,8 +47,9 @@ const main = async () => {
         console.log("Cómo está el clima: ", clima.desc.green);
 
         break;
+        
       case 2:
-        busquedas.historial.forEach((lugar, i) => {
+        busquedas.historialCapitalizado.forEach((lugar, i) => {
           const idx = `${i + 1}`.green;
           console.log(`${idx} ${lugar}`);
         });
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 const axios = require("axios");
 
 class Busquedas {
   historial = [];
-  dbPath = './db/database.json';
+  dbPath = "./db/database.json";
 
   constructor() {
     // todo: leer db si existe
+    this.leerDB();
+  }
+  get historialCapitalizado() {
+    return this.historial.map((lugar) => {
+      let palabras = lugar.split(" ");
+      palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+
+      return palabras.join(' ')
+    });
+
   }
   get paramsMapbox() {
     return {
@@ -72,17 +82,24 @@ class Busquedas {
     }
     this.historial.unshift(lugar.toLocaleLowerCase());
     // grabar en db
-    this.guardarDB()
+    this.guardarDB();
   }
 
   guardarDB() {
     const payload = {
-      historial: this.historial
-    }
-    fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+      historial: this.historial,
+    };
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
   }
 
-  leerDB() {}
+  leerDB() {
+    if (!fs.existsSync(this.dbPath)) return;
+
+    const info = fs.readFileSync(this.dbPath, { encoding: "utf-8" });
+    const data = JSON.parse(info);
+
+    this.historial = data.historial;
+  }
 }
 
 module.exports = Busquedas;
