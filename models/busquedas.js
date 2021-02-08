d77e2f9e5dea29cadff40751e1d95bb219b7bc47
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-08 12:39:46 -0300

    llamando nuestra api mpbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,15 +1,34 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class Busquedas {
   historial = ["santiago", "bogota", "buenos aires"];
   constructor() {
     // todo: leer db si existe
+  
+  }
+  get paramsMapbox(){
+    return{
+      'access_token': "pk.eyJ1IjoiY3Jpc3RpYW5wYXJyYSIsImEiOiJja2t3cHF6NzIyN3g1MnBtcnBvdGw4enE4In0.gcfWIHlMil_4EymzJwNMHQ",
+      'limit': 5,
+      'language': "es"
+    }
   }
   async ciudad(lugar = "") {
-    // peticion http
-    // console.log('ciudad',lugar);
-    const resp = await axios.get('https://reqres.in/api/users?page=2')
-    console.log(resp.data)
+    try {
+      // peticion http
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox
+      });
+      // console.log('ciudad',lugar);
+      const resp = await instance.get();
+      console.log(resp.data);
+
+      return [];
+
+    } catch (error) {
+      return [];
+    }
 
     return []; // retornar los lugares
   }
